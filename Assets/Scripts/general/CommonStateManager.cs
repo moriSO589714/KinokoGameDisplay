15bc4088ee539db01fb6ee5ee89708c8c8cbd9a8
nametakeman <132148860+nametakeman@users.noreply.github.com>
2025-10-03 23:27:06 +0900

    [Add]StateManager完成&FIrstLoadScene

--- Assets/Scripts/general/CommonStateManager.cs
+++ Assets/Scripts/general/CommonStateManager.cs
@@ -1,5 +1,7 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public enum CommonStates
@@ -10,5 +12,32 @@ public enum CommonStates
 }
 public class CommonStateManager : BasedSingleton<CommonStateManager>
 {
+    //現在の状態
+    [HideInInspector] public CommonStates _currentState { get; private set; }
+    //ステート切り替え時に実行するメソッド
+    private List<Func<CancellationToken, UniTask>> _onChangeActions = new();
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _currentState = CommonStates.Load;
+    }
+
+    public async UniTask SetCurrentState(CommonStates staet)
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        
+        //現在のステートの終了処理をする。
+        await UniTask.WhenAll(_onChangeActions.Select(f => f(cts.Token)));
+        //タスクの破棄
+        cts.Cancel();
+        _onChangeActions = new();
+        
+        
+    }
     
+    public void AddOnChangeAction(Func<CancellationToken, UniTask> action)
+    {
+        _onChangeActions.Add(action);
+    }
 }
