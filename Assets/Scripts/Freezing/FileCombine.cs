0bc5d9dcc278d9aea92745f3da5a65e8b8b8ea36
nametakeman <132148860+nametakeman@users.noreply.github.com>
2026-02-16 09:28:52 +0900

    [Change Encoding] ShiftJis to Unicode

---
+++ Assets/Scripts/Freezing/FileCombine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class FileCombine : MonoBehaviour
+{
+    /// <summary>
+    /// 分割されたZIPファイルを結合するメソッド
+    /// </summary>
+    /// <param name="mergedFilesPath">結合されるファイル群が置かれているディレクトリのパス</param>
+    public void MergeSplitedFile(string mergedFilesPath)
+    {
+        //対象ディレクトリ内のファイルのパスを取得してくる
+        string[] splitedFiles = Directory.GetFiles(mergedFilesPath);
+        //取得したファイルパス群から、語尾の数値でソートを行う(数値以外の部分は共通しているため、単純にソートを行うことが可能)
+        //ToArray()はキャッシュ化してLINQの遅延実行を無視するため
+        string[] sortedFiles = splitedFiles.OrderBy(f => f).ToArray();
+
+
+    }
+}
